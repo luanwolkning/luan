85ff0117aa88c3462062e25a2938e48e1db13528
luanwolkning <wolkningluan@gmail.com>
2024-10-31 09:41:40 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -15,3 +15,8 @@ criaCartao(
   'O que é uma função?',
   'Uma função é um bloco de código que executa alguma tarefa'
 )
+criaCartao(
+    "Língua Inglesa",
+    "Como se diz oi em Inglês?",
+    "Oi em inglês se diz HI (RAI)"
+)
